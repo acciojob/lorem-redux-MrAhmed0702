2731e8b188197b7af87c77fde0a340c941a3d169
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-11-19 23:21:28 +0530

    are we done now ?

--- src/store/store.js
+++ src/store/store.js
@@ -2,4 +2,6 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { loremReducer } from "./reducer";
 
-export const store = createStore(loremReducer, applyMiddleware(thunk));
+const store = createStore(loremReducer, applyMiddleware(thunk));
+
+export default store;
