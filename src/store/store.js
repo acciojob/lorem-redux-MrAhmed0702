230a4dd4773e1aea9b31ee77c20747168323083c
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-11-19 23:01:30 +0530

    are we done ?

---
+++ src/store/store.js
@@ -0,0 +1,5 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { loremReducer } from "./reducer";
+
+export const store = createStore(loremReducer, applyMiddleware(thunk));
