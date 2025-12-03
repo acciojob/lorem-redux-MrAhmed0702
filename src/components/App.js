33596bd420c580fc0632fd108bb41460dccb7533
MrAhmed0702 <amochi670@rku.ac.in>
2025-12-03 06:06:36 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <div>
-      <h1>A short Narration of Lorem Ipsum</h1>
+      <h1>A short Naration of Lorem Ipsum</h1>
 
       {loading && <h4>Loading...</h4>}
 
