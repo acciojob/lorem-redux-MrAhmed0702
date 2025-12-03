32b17eeb22510ffeb295b45215debfcc0790a3f8
MrAhmed0702 <amochi670@rku.ac.in>
2025-12-03 05:53:22 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'regenerator-runtime/runtime';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import store from './store/store';
