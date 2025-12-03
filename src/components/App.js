eef17e439ba68fe028146fd47ddc90b91f3514fd
MrAhmed0702 <amochi670@rku.ac.in>
2025-12-03 06:03:52 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchPosts } from "./store/actions";
+import { fetchPosts } from "../store/actions";
 
 function App() {
   const dispatch = useDispatch();
